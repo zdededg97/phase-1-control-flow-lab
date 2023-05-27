588361a31bc0f68d7eb9af58206a665676638767
Edgar Hernandez <zdededg@gmail.com>
2023-05-27 08:12:32 -0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let response;
+  if (feet <= 400) {
+    response = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    response = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else {
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
